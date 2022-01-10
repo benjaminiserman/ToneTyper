222f786c3dee210863ca4c4fc4b32b3682b16bee
Benjamin E. Iserman <winggar1228@gmail.com>
2022-01-10 16:11:07 -0600

    change default hotkey

--- ToneTyper/Config.cs
+++ ToneTyper/Config.cs
@@ -3,7 +3,7 @@ using WK.Libraries.HotkeyListenerNS;
 
 internal record Config
 {
-	public Hotkey ToggleHotkey { get; set; } = new(Keys.Alt, Keys.E);
+	public Hotkey ToggleHotkey { get; set; } = new(Keys.Alt, Keys.P);
 
 	public VMode VMode { get; set; } = VMode.DoubleU;
 }
